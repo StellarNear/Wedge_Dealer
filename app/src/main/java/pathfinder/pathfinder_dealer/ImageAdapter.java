2f08a66c0211f880446176c2203983d6b7c7d44b
StellarNear <jeremie.chatron@gmail.com>
2017-08-24 00:54:32 +0200

    version stable - relative layout - addition complete du jet d'attaque OK - checkbox OK - random jet_att OK - dés 4,5,6 position OK
    TODO :
    -multishot dynamic
    -  box qui crit
    
    PART 2 : les dégats
    -lancement des dées de degats
    -addition des degats par element

--- app/src/main/java/pathfinder/pathfinder_dealer/ImageAdapter.java
+++ app/src/main/java/pathfinder/pathfinder_dealer/ImageAdapter.java
@@ -6,29 +6,123 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ImageAdapter  extends BaseAdapter {
     private Context mContext;
 
     // Keep all Images in array
-    public Integer[] mThumbIds = {
-            R.mipmap.d20_1,R.mipmap.d20_2,R.mipmap.d20_3,R.mipmap.d20_4,R.mipmap.d20_5,
-            R.mipmap.d20_6
-    };
+    public List<Integer> mThumbIds;
+
+    public static final Map<String,Integer> dice_map= new HashMap<>();
+    static {
+        dice_map.put("d20_1",R.mipmap.d20_1);
+        dice_map.put("d20_10",R.mipmap.d20_10);
+        dice_map.put("d20_11",R.mipmap.d20_11);
+        dice_map.put("d20_12",R.mipmap.d20_12);
+        dice_map.put("d20_13",R.mipmap.d20_13);
+        dice_map.put("d20_14",R.mipmap.d20_14);
+        dice_map.put("d20_15",R.mipmap.d20_15);
+        dice_map.put("d20_16",R.mipmap.d20_16);
+        dice_map.put("d20_17",R.mipmap.d20_17);
+        dice_map.put("d20_18",R.mipmap.d20_18);
+        dice_map.put("d20_19",R.mipmap.d20_19);
+        dice_map.put("d20_2",R.mipmap.d20_2);
+        dice_map.put("d20_20",R.mipmap.d20_20);
+        dice_map.put("d20_3",R.mipmap.d20_3);
+        dice_map.put("d20_4",R.mipmap.d20_4);
+        dice_map.put("d20_5",R.mipmap.d20_5);
+        dice_map.put("d20_6",R.mipmap.d20_6);
+        dice_map.put("d20_7",R.mipmap.d20_7);
+        dice_map.put("d20_8",R.mipmap.d20_8);
+        dice_map.put("d20_9",R.mipmap.d20_9);
+        dice_map.put("d6_1",R.mipmap.d6_1);
+        dice_map.put("d6_2",R.mipmap.d6_2);
+        dice_map.put("d6_3",R.mipmap.d6_3);
+        dice_map.put("d6_4",R.mipmap.d6_4);
+        dice_map.put("d6_5",R.mipmap.d6_5);
+        dice_map.put("d6_6",R.mipmap.d6_6);
+        dice_map.put("d8_1",R.mipmap.d8_1);
+        dice_map.put("d8_2",R.mipmap.d8_2);
+        dice_map.put("d8_3",R.mipmap.d8_3);
+        dice_map.put("d8_4",R.mipmap.d8_4);
+        dice_map.put("d8_5",R.mipmap.d8_5);
+        dice_map.put("d8_6",R.mipmap.d8_6);
+        dice_map.put("d8_7",R.mipmap.d8_7);
+        dice_map.put("d8_8",R.mipmap.d8_8);
+        dice_map.put("feu_d10_1",R.mipmap.feu_d10_1);
+        dice_map.put("feu_d10_10",R.mipmap.feu_d10_10);
+        dice_map.put("feu_d10_2",R.mipmap.feu_d10_2);
+        dice_map.put("feu_d10_3",R.mipmap.feu_d10_3);
+        dice_map.put("feu_d10_4",R.mipmap.feu_d10_4);
+        dice_map.put("feu_d10_5",R.mipmap.feu_d10_5);
+        dice_map.put("feu_d10_6",R.mipmap.feu_d10_6);
+        dice_map.put("feu_d10_7",R.mipmap.feu_d10_7);
+        dice_map.put("feu_d10_8",R.mipmap.feu_d10_8);
+        dice_map.put("feu_d10_9",R.mipmap.feu_d10_9);
+        dice_map.put("feu_d6_1",R.mipmap.feu_d6_1);
+        dice_map.put("feu_d6_2",R.mipmap.feu_d6_2);
+        dice_map.put("feu_d6_3",R.mipmap.feu_d6_3);
+        dice_map.put("feu_d6_4",R.mipmap.feu_d6_4);
+        dice_map.put("feu_d6_5",R.mipmap.feu_d6_5);
+        dice_map.put("feu_d6_6",R.mipmap.feu_d6_6);
+        dice_map.put("foudre_d10_1",R.mipmap.foudre_d10_1);
+        dice_map.put("foudre_d10_10",R.mipmap.foudre_d10_10);
+        dice_map.put("foudre_d10_2",R.mipmap.foudre_d10_2);
+        dice_map.put("foudre_d10_3",R.mipmap.foudre_d10_3);
+        dice_map.put("foudre_d10_4",R.mipmap.foudre_d10_4);
+        dice_map.put("foudre_d10_5",R.mipmap.foudre_d10_5);
+        dice_map.put("foudre_d10_6",R.mipmap.foudre_d10_6);
+        dice_map.put("foudre_d10_7",R.mipmap.foudre_d10_7);
+        dice_map.put("foudre_d10_8",R.mipmap.foudre_d10_8);
+        dice_map.put("foudre_d10_9",R.mipmap.foudre_d10_9);
+        dice_map.put("foudre_d6_1",R.mipmap.foudre_d6_1);
+        dice_map.put("foudre_d6_2",R.mipmap.foudre_d6_2);
+        dice_map.put("foudre_d6_3",R.mipmap.foudre_d6_3);
+        dice_map.put("foudre_d6_4",R.mipmap.foudre_d6_4);
+        dice_map.put("foudre_d6_5",R.mipmap.foudre_d6_5);
+        dice_map.put("foudre_d6_6",R.mipmap.foudre_d6_6);
+        dice_map.put("froid_d10_1",R.mipmap.froid_d10_1);
+        dice_map.put("froid_d10_10",R.mipmap.froid_d10_10);
+        dice_map.put("froid_d10_2",R.mipmap.froid_d10_2);
+        dice_map.put("froid_d10_3",R.mipmap.froid_d10_3);
+        dice_map.put("froid_d10_4",R.mipmap.froid_d10_4);
+        dice_map.put("froid_d10_5",R.mipmap.froid_d10_5);
+        dice_map.put("froid_d10_6",R.mipmap.froid_d10_6);
+        dice_map.put("froid_d10_7",R.mipmap.froid_d10_7);
+        dice_map.put("froid_d10_8",R.mipmap.froid_d10_8);
+        dice_map.put("froid_d10_9",R.mipmap.froid_d10_9);
+        dice_map.put("froid_d6_1",R.mipmap.froid_d6_1);
+        dice_map.put("froid_d6_2",R.mipmap.froid_d6_2);
+        dice_map.put("froid_d6_3",R.mipmap.froid_d6_3);
+        dice_map.put("froid_d6_4",R.mipmap.froid_d6_4);
+        dice_map.put("froid_d6_5",R.mipmap.froid_d6_5);
+        dice_map.put("froid_d6_6",R.mipmap.froid_d6_6);
+
+    }
+
 
     // Constructor
-    public ImageAdapter(Context c){
+    public ImageAdapter(Context c,List<String> dice_list ){
+        mThumbIds = new ArrayList<>();
+        for (String each : dice_list)   {
+            mThumbIds.add(dice_map.get(each));
+        }
+
         mContext = c;
     }
 
     @Override
     public int getCount() {
-        return mThumbIds.length;
+        return mThumbIds.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return mThumbIds[position];
+        return mThumbIds.get(position);
     }
 
     @Override
@@ -46,7 +140,7 @@ public class ImageAdapter  extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
-        imageView.setImageResource(mThumbIds[position]);
+        imageView.setImageResource(mThumbIds.get(position));
         return imageView;
     }
 
--- app/src/main/java/pathfinder/pathfinder_dealer/MainActivity.java
+++ app/src/main/java/pathfinder/pathfinder_dealer/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,9 +22,14 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     boolean shouldExecuteOnResume;
+
+    Integer n_att;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        jet_att_print("base");
+        jet_att_print();
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -47,85 +53,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                Snackbar.make(view, "Replace with your own action "+"lol"+settings.getString("jet_att",getResources().getString(R.string.jet_att_def)), Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Lancement des dés en cours... "+"\nVos "+ n_att +" attaques ont été faites.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-                //set_grid();
-
-                jet_att_print("ajout");
-
+                set_grid();
 
                 if (settings.getBoolean("feu_nourri_switch",getResources().getBoolean(R.bool.feu_nourri_switch_def)))  {
                     TextView multi_text = (TextView) findViewById(R.id.multishot);
                     multi_text.setVisibility(View.VISIBLE);
                 }
 
+                CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkBox6);
+                checkBox6.setVisibility(View.INVISIBLE);
+                CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
+                checkBox5.setVisibility(View.INVISIBLE);
 
                 TextView hit_text = (TextView) findViewById(R.id.hit_text);
                 hit_text.setVisibility(View.VISIBLE);
 
+                set_checkbox_hit();
 
-                LinearLayout check_lin = (LinearLayout) findViewById(R.id.grid_check_id);
-
-                // 6 attaques
-                check_lin.setPadding(50,0,0,0);
-                check_lin.setWeightSum(6);
-                CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
-                CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
-                CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
-                CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
-                CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
-                CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkBox6);
-                checkBox1.setVisibility(View.VISIBLE);
-                checkBox2.setVisibility(View.VISIBLE);
-                checkBox3.setVisibility(View.VISIBLE);
-                checkBox4.setVisibility(View.VISIBLE);
-                checkBox5.setVisibility(View.VISIBLE);
-                checkBox6.setVisibility(View.VISIBLE);
-
-                /* 5 attaques
-                check_lin.setPadding(120,0,0,0);
-                check_lin.setWeightSum(5);
-                CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
-                CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
-                CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
-                CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
-                CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
-
-                checkBox1.setVisibility(View.VISIBLE);
-                checkBox2.setVisibility(View.VISIBLE);
-                checkBox3.setVisibility(View.VISIBLE);
-                checkBox4.setVisibility(View.VISIBLE);
-                checkBox5.setVisibility(View.VISIBLE);
-                */
-                /*4 attaques
-                check_lin.setPadding(250,0,0,0);
-                check_lin.setWeightSum(4);
-                CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
-                CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
-                CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
-                CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
-
-                checkBox1.setVisibility(View.VISIBLE);
-                checkBox2.setVisibility(View.VISIBLE);
-                checkBox3.setVisibility(View.VISIBLE);
-                checkBox4.setVisibility(View.VISIBLE);
-                */
 
             }});
     }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
         if(shouldExecuteOnResume){
-            jet_att_print("base");
+            jet_att_print();
         } else {
             shouldExecuteOnResume = true;
         }
     }
 
-    private void jet_att_print(String mode) {
+    private void jet_att_print() {
 
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
@@ -188,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         int length = list_att.size();
-        int n_att = length;
+        n_att = length;
 
         for(int i=0; i<n_att; i++){
             list_att.set(i,list_att.get(i) + 3*thor + 1*predi  + val_dex + magic*magic_val + neuf_m + epic_val);
@@ -199,6 +163,8 @@ public class MainActivity extends AppCompatActivity {
             n_att=n_att+1;
             list_att.add(0,max_att_jet);
         }
+
+
 
         if (settings.getBoolean("tir_rapide",getResources().getBoolean(R.bool.tir_rapide_switch_def))) {
             n_att=n_att+1;
@@ -220,29 +186,204 @@ public class MainActivity extends AppCompatActivity {
 
 
         StringBuilder affichage_att = new StringBuilder();
+        String delim2=" / +";
         for (Integer each : list_att) {
-            affichage_att.append(" / +").append(String.valueOf(each));
+            affichage_att.append(delim2).append(String.valueOf(each));
         }
 
-        String affichage_att_str = affichage_att.substring(delim.length()+1);
+        String affichage_att_str = affichage_att.substring(delim2.length()-1);
 
-        if (mode.equals("base")) {
-            TextView text_curent_att = (TextView) findViewById(R.id.jet_att_textbox);
-            text_curent_att.setText( n_att + " attaques\n" +affichage_att_str);
-        }   else if (mode.equals("ajout")) {
-            TextView text_curent_att = (TextView) findViewById(R.id.jet_att_ajout_textbox);
-            text_curent_att.setVisibility(View.VISIBLE);
-            text_curent_att.setText(affichage_att_str);
-        }
+
+        TextView text_curent_att = (TextView) findViewById(R.id.jet_att_textbox);
+        text_curent_att.setText( n_att + " attaques\n" +affichage_att_str);
+
     }
+
+    private void jet_att_print(Integer jet1,Integer jet2,Integer jet3,Integer jet4,Integer jet5,Integer jet6) {
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        Integer[] all_jet= new Integer[]{jet1,jet2,jet3,jet4,jet5,jet6};
+
+        //calcul des valeurs d'attaque
+
+        String att_base = settings.getString("jet_att",getResources().getString(R.string.jet_att_def));
+        String delim = ",";
+
+        String[] list_att_base_string = att_base.split(delim);
+
+        List<Integer> list_att_base = new ArrayList<Integer>();
+
+
+        for (String each : list_att_base_string) {
+            list_att_base.add(Integer.parseInt(each));
+        }
+
+        List<Integer> list_att = new ArrayList<Integer>(list_att_base);
+
+        Integer thor;
+        Integer predi;
+        Integer neuf_m;
+        Integer magic;
+
+        if (settings.getBoolean("thor_switch",getResources().getBoolean(R.bool.thor_switch_def))) {
+            thor = 1;
+        }  else { thor = 0; }
+        if (settings.getBoolean("predil_switch",getResources().getBoolean(R.bool.predil_switch_def)))  {
+            predi = 1;
+        }    else { predi = 0; }
+        if (settings.getBoolean("neuf_m_switch",getResources().getBoolean(R.bool.neuf_m_switch_def)))  {
+            neuf_m = 1;
+        }    else { neuf_m = 0; }
+        if (settings.getBoolean("magic_switch",getResources().getBoolean(R.bool.magic_switch_def)))  {
+            magic = 1;
+        }    else { magic = 0; }
+
+
+        String val_dex_str = settings.getString("mod_dex",getResources().getString(R.string.mod_dex_def));
+        Integer val_dex = Integer.parseInt(val_dex_str);
+
+        String magic_val_str = settings.getString("magic_val",getResources().getString(R.string.magic_val_def));
+        Integer magic_val = Integer.parseInt(magic_val_str);
+
+        String epic_str = settings.getString("epic_val",getResources().getString(R.string.epic_val_def));
+        Integer epic_val = Integer.parseInt(epic_str);
+
+
+        String prouesse_str = settings.getString("prouesse_val",getResources().getString(R.string.prouesse_def));
+        Integer prouesse = Integer.parseInt(prouesse_str);
+
+        String prouesse_attrib_str = settings.getString("prouesse_attrib",getResources().getString(R.string.prouesse_attrib_def));
+        Integer prouesse_attrib = Integer.parseInt(prouesse_attrib_str);
+
+        list_att.set(prouesse_attrib-1,prouesse+list_att.get(prouesse_attrib-1));
+
+        int max_att_jet = list_att_base.get(0) + 3*thor + predi  + val_dex + magic*magic_val + neuf_m + epic_val;
+
+
+        int length = list_att.size();
+        n_att = length;
+
+        for(int i=0; i<n_att; i++){
+            list_att.set(i,list_att.get(i) + 3*thor + 1*predi  + val_dex + magic*magic_val + neuf_m + epic_val);
+        }
+
+
+        if (settings.getBoolean("rapid_enchant_switch",getResources().getBoolean(R.bool.rapid_enchant_switch_def))) {
+            n_att=n_att+1;
+            list_att.add(0,max_att_jet);
+        }
+
+
+
+        if (settings.getBoolean("tir_rapide",getResources().getBoolean(R.bool.tir_rapide_switch_def))) {
+            n_att=n_att+1;
+            list_att.add(0,max_att_jet);
+            for(int i=0; i<n_att; i++){
+                list_att.set(i,list_att.get(i) -2);
+            }
+        }
+
+        if (settings.getBoolean("viser",getResources().getBoolean(R.bool.viser_switch_def))) {
+            for(int i=0; i<n_att; i++){
+                String malus_viser_str = settings.getString("viser_val",getResources().getString(R.string.viser_val_def));
+                Integer malus_viser = Integer.parseInt(malus_viser_str);
+                list_att.set(i,list_att.get(i) -malus_viser);
+            }
+        }
+
+        for(int i=0; i<n_att; i++){
+            list_att.set(i,list_att.get(i) + all_jet[i]);
+        }
+
+        StringBuilder affichage_att = new StringBuilder();
+        String delim2="  /  ";
+        for (Integer each : list_att) {
+            affichage_att.append(delim2).append(String.valueOf(each));
+        }
+
+        String affichage_att_str = affichage_att.substring(delim2.length()-1);
+
+        TextView text_curent_att = (TextView) findViewById(R.id.jet_att_ajout_textbox);
+        text_curent_att.setVisibility(View.VISIBLE);
+        text_curent_att.setText(affichage_att_str);
+
+    }
+
 
     private void set_grid() {
         //setContentView(R.layout.grid_map);  C'est ca qui efface toute la view
         GridView grid = (GridView) findViewById(R.id.grid_dice_id);
-        grid.setNumColumns(6);
-        // Instance of ImageAdapter Class
-        grid.setAdapter(new ImageAdapter(this));
 
+        // Instance of ImageAdapter Class
+
+        Integer jet1= rand(20),jet2= rand(20),jet3= rand(20),jet4= rand(20),jet5 = rand(20) ,jet6 = rand(20);
+        String jet1_str=String.valueOf(jet1),jet2_str=String.valueOf(jet2),jet3_str=String.valueOf(jet3),
+                jet4_str=String.valueOf(jet4),jet5_str=String.valueOf(jet5),jet6_str=String.valueOf(jet6);
+
+        switch (n_att) {
+            case 6:
+                List<String> dice_list6 = new ArrayList<>(Arrays.asList("d20_" + jet1_str, "d20_" + jet2_str, "d20_" + jet3_str, "d20_" + jet4_str, "d20_" + jet5_str, "d20_" + jet6_str));
+                grid.setAdapter(new ImageAdapter(this, dice_list6));
+                GridView dice_lin6 = (GridView) findViewById(R.id.grid_dice_id);
+                dice_lin6.setNumColumns(6);
+
+                break;
+            case 5:
+                List<String> dice_list5 = new ArrayList<>(Arrays.asList("d20_" + jet1_str, "d20_" + jet2_str, "d20_" + jet3_str, "d20_" + jet4_str, "d20_" + jet5_str));
+                grid.setAdapter(new ImageAdapter(this, dice_list5));
+                GridView dice_lin5 = (GridView) findViewById(R.id.grid_dice_id);
+                dice_lin5.setNumColumns(5);
+                dice_lin5.setPadding(25,0,0,0);
+                break;
+            case 4:
+                List<String> dice_list4 = new ArrayList<>(Arrays.asList("d20_" + jet1_str, "d20_" + jet2_str, "d20_" + jet3_str, "d20_" + jet4_str));
+                grid.setAdapter(new ImageAdapter(this, dice_list4));
+                GridView dice_lin4 = (GridView) findViewById(R.id.grid_dice_id);
+                dice_lin4.setNumColumns(4);
+                dice_lin4.setPadding(65,0,0,0);
+                break;
+
+        }
+        jet_att_print(jet1,jet2,jet3,jet4,jet5,jet6);
+
+
+
+
+    }
+
+    private Integer rand(Integer dice) {
+        Random r = new Random();
+        int jet = r.nextInt(dice)+1;
+        return jet;
+    }
+
+    private void set_checkbox_hit() {
+        LinearLayout check_lin = (LinearLayout) findViewById(R.id.grid_check_id);
+
+        check_lin.setPadding(-100*n_att+650,0,0,0);
+        check_lin.setWeightSum(n_att);
+
+        switch(n_att) {
+            case 6:
+                CheckBox checkBox6 = (CheckBox) findViewById(R.id.checkBox6);
+                checkBox6.setVisibility(View.VISIBLE);
+
+            case 5:
+                CheckBox checkBox5 = (CheckBox) findViewById(R.id.checkBox5);
+                checkBox5.setVisibility(View.VISIBLE);
+
+            case 4:
+                CheckBox checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+                CheckBox checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+                CheckBox checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+                CheckBox checkBox4 = (CheckBox) findViewById(R.id.checkBox4);
+                checkBox1.setVisibility(View.VISIBLE);
+                checkBox2.setVisibility(View.VISIBLE);
+                checkBox3.setVisibility(View.VISIBLE);
+                checkBox4.setVisibility(View.VISIBLE);
+
+
+        }
     }
 
 
