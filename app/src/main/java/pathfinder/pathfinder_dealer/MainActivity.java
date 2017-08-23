67a5f60f75d470b6da767ae6e2a088720345eed6
StellarNear <jeremie.chatron@gmail.com>
2017-08-23 16:43:01 +0200

    version stable - relative layout - addition complete du jet d'attaque OK
    TODO :
    
    - rand + affichage rand dé
    
    - box qui touche qui crit
    
    PART 2 : les dégats
    -lancement des dées de degats
    -addition des degats par element

--- app/src/main/java/pathfinder/pathfinder_dealer/MainActivity.java
+++ app/src/main/java/pathfinder/pathfinder_dealer/MainActivity.java
@@ -14,12 +14,16 @@ import android.view.MenuItem;
 import android.widget.GridView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    boolean shouldExecuteOnResume;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        shouldExecuteOnResume = false;
         // reset les pref au defaut
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = prefs.edit();
@@ -31,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        jet_att_print();
+        jet_att_print("base");
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -44,29 +48,141 @@ public class MainActivity extends AppCompatActivity {
 
                 set_grid();
 
+                jet_att_print("ajout");
+
+                TextView hit_text = (TextView) findViewById(R.id.hit_text);
+                hit_text.setVisibility(View.VISIBLE);
+
             }});
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        jet_att_print();
-        //set_grid();
+        if(shouldExecuteOnResume){
+            jet_att_print("base");
+        } else {
+            shouldExecuteOnResume = true;
+        }
     }
 
-    private void jet_att_print() {
+    private void jet_att_print(String mode) {
+
         SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        TextView text_curent_att = (TextView) findViewById(R.id.jet_att_textbox);
-        text_curent_att.setText(settings.getString("jet_att",getResources().getString(R.string.jet_att_def)));
+
+
+        //calcul des valeurs d'attaque
+
+        String att_base = settings.getString("jet_att",getResources().getString(R.string.jet_att_def));
+        String delim = ",";
+
+        String[] list_att_base_string = att_base.split(delim);
+
+        List<Integer> list_att_base = new ArrayList<Integer>();
+
+
+        for (String each : list_att_base_string) {
+            list_att_base.add(Integer.parseInt(each));
+        }
+
+        List<Integer> list_att = new ArrayList<Integer>(list_att_base);
+
+        Integer thor;
+        Integer predi;
+        Integer neuf_m;
+        Integer magic;
+
+        if (settings.getBoolean("thor_switch",getResources().getBoolean(R.bool.thor_switch_def))) {
+            thor = 1;
+        }  else { thor = 0; }
+        if (settings.getBoolean("predil_switch",getResources().getBoolean(R.bool.predil_switch_def)))  {
+            predi = 1;
+        }    else { predi = 0; }
+        if (settings.getBoolean("neuf_m_switch",getResources().getBoolean(R.bool.neuf_m_switch_def)))  {
+            neuf_m = 1;
+        }    else { neuf_m = 0; }
+        if (settings.getBoolean("magic_switch",getResources().getBoolean(R.bool.magic_switch_def)))  {
+            magic = 1;
+        }    else { magic = 0; }
+
+
+        String val_dex_str = settings.getString("mod_dex",getResources().getString(R.string.mod_dex_def));
+        Integer val_dex = Integer.parseInt(val_dex_str);
+
+        String magic_val_str = settings.getString("magic_val",getResources().getString(R.string.magic_val_def));
+        Integer magic_val = Integer.parseInt(magic_val_str);
+
+        String epic_str = settings.getString("epic_val",getResources().getString(R.string.epic_val_def));
+        Integer epic_val = Integer.parseInt(epic_str);
+
+
+        String prouesse_str = settings.getString("prouesse_val",getResources().getString(R.string.prouesse_def));
+        Integer prouesse = Integer.parseInt(prouesse_str);
+
+        String prouesse_attrib_str = settings.getString("prouesse_attrib",getResources().getString(R.string.prouesse_attrib_def));
+        Integer prouesse_attrib = Integer.parseInt(prouesse_attrib_str);
+
+        list_att.set(prouesse_attrib-1,prouesse+list_att.get(prouesse_attrib-1));
+
+        int max_att_jet = list_att_base.get(0) + 3*thor + predi  + val_dex + magic*magic_val + neuf_m + epic_val;
+
+
+
+        int length = list_att.size();
+        int n_att = length;
+
+        for(int i=0; i<n_att; i++){
+            list_att.set(i,list_att.get(i) + 3*thor + 1*predi  + val_dex + magic*magic_val + neuf_m + epic_val);
+        }
+
+
+        if (settings.getBoolean("rapid_enchant_switch",getResources().getBoolean(R.bool.rapid_enchant_switch_def))) {
+            n_att=n_att+1;
+            list_att.add(0,max_att_jet);
+        }
+
+        if (settings.getBoolean("tir_rapide",getResources().getBoolean(R.bool.tir_rapide_switch_def))) {
+            n_att=n_att+1;
+            list_att.add(0,max_att_jet);
+            for(int i=0; i<n_att; i++){
+                list_att.set(i,list_att.get(i) -2);
+            }
+        }
+
+        if (settings.getBoolean("viser",getResources().getBoolean(R.bool.viser_switch_def))) {
+            for(int i=0; i<n_att; i++){
+                String malus_viser_str = settings.getString("viser_val",getResources().getString(R.string.viser_val_def));
+                Integer malus_viser = Integer.parseInt(malus_viser_str);
+                list_att.set(i,list_att.get(i) -malus_viser);
+            }
+        }
+
+
+
+
+        StringBuilder affichage_att = new StringBuilder();
+        for (Integer each : list_att) {
+            affichage_att.append(" / +").append(String.valueOf(each));
+        }
+
+        String affichage_att_str = affichage_att.substring(delim.length()+1);
+
+        if (mode.equals("base")) {
+            TextView text_curent_att = (TextView) findViewById(R.id.jet_att_textbox);
+            text_curent_att.setText( n_att + " attaques\n" +affichage_att_str);
+        }   else if (mode.equals("ajout")) {
+            TextView text_curent_att = (TextView) findViewById(R.id.jet_att_ajout_textbox);
+            text_curent_att.setVisibility(View.VISIBLE);
+            text_curent_att.setText(affichage_att_str);
+        }
     }
 
     private void set_grid() {
         //setContentView(R.layout.grid_map);  C'est ca qui efface toute la view
-        GridView grid = (GridView) findViewById(R.id.grid_id);
+        GridView grid = (GridView) findViewById(R.id.grid_dice_id);
         grid.setNumColumns(6);
         // Instance of ImageAdapter Class
         grid.setAdapter(new ImageAdapter(this));
-
 
     }
 
